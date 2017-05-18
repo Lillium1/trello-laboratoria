40a399114959b8b6be5ab61631fbac8936cf803f
Paulina Gonzalez C <pgc.lacroix@gmail.com>
2017-05-18 06:19:37 -0400

    falta las nuevas tareas y validaciones...etc

--- js/main.js
+++ js/main.js
@@ -1,29 +1,87 @@
 var holder=document.getElementById("holder");
+holder.addEventListener("click", principal)
+
+function principal (){
+    function mostrarBoton(){
+        document.getElementById('oculto').style.display = 'block'
+
+    }
+    mostrarBoton();
+
+    function listaTareas(){
+        var guardar=document.getElementById("boton");
+            guardar.addEventListener("click", secundario)
+
+            function secundario(){
+                
+                var campo=document.getElementById("holder").value;
+                var nodoCampo=document.createTextNode(campo);
+                var listado=document.createElement("div")
+                    listado.setAttribute("id","listado")
+                var textarea=document.createElement("textarea");
+                var contenedor=document.getElementById("listaTareas")
+                    parrafo.appendChild(nodoCampo); 
+                    listado.appendChild(parrafo);
+                    contenedor.appendChild(listado)
+                
+
+                function moverCaja(){
+                    document.getElementById('contenedor').style.float = 'right'
+                };
+                moverCaja()
+
+            };
+    }
+    listaTareas()
+};
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+var holder=document.getElementById("holder");
 var box1=document.getElementById("box1");
 holder.addEventListener("click",function(){
         var divo=box1.parentNode;
         var textarea=document.createElement("textarea")
-        	textarea.setAttribute("id","areatext");
-			divo.appendChild(textarea);
+            textarea.setAttribute("id","areatext");
+            divo.appendChild(textarea);
 
     function listaTareas(){
         var guardar=document.getElementById("boton");
 
-        	guardar.addEventListener("click", function(){
-	        var campo=document.getElementById("areatext").value;
-	        var nodoCampo=document.createTextNode(campo);
-	        var parrafo=document.createElement("p");
-	        var contenedor=document.getElementById("contenedor")
-	        	parrafo.appendChild(nodoCampo);
-	        	contenedor.appendChild(parrafo);
+            guardar.addEventListener("click", function(){
+            var campo=document.getElementById("areatext").value;
+            var nodoCampo=document.createTextNode(campo);
+            var parrafo=document.createElement("p");
+            var contenedor=document.getElementById("contenedor")
+                parrafo.appendChild(nodoCampo);
+                contenedor.appendChild(parrafo);
         
-        	})
+            })
 
         function eliminar(){
-	        var elimina=document.getElementById("borra");
-	        	elimina.removeChild(holder);
-	   		};
-    	eliminar();
+            var elimina=document.getElementById("borra");
+                elimina.removeChild(holder);
+            };
+        eliminar();
 
 
     }
@@ -31,3 +89,4 @@ holder.addEventListener("click",function(){
     
 });
 
+*/
