7e0e4ed7af7396b3ea069c30d34d709ebf51a99a
Paulina Gonzalez C <pgc.lacroix@gmail.com>
2017-05-18 09:52:33 -0400

    se arregla un problema con el p de parrafo

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ function principal (){
                 
                 var campo=document.getElementById("holder").value;
                 var nodoCampo=document.createTextNode(campo);
+                var parrafo=document.createElement("p")
                 var listado=document.createElement("div")
                     listado.setAttribute("id","listado")
                 var textarea=document.createElement("textarea");
@@ -34,59 +35,3 @@ function principal (){
     }
     listaTareas()
 };
-
-
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-var holder=document.getElementById("holder");
-var box1=document.getElementById("box1");
-holder.addEventListener("click",function(){
-        var divo=box1.parentNode;
-        var textarea=document.createElement("textarea")
-            textarea.setAttribute("id","areatext");
-            divo.appendChild(textarea);
-
-    function listaTareas(){
-        var guardar=document.getElementById("boton");
-
-            guardar.addEventListener("click", function(){
-            var campo=document.getElementById("areatext").value;
-            var nodoCampo=document.createTextNode(campo);
-            var parrafo=document.createElement("p");
-            var contenedor=document.getElementById("contenedor")
-                parrafo.appendChild(nodoCampo);
-                contenedor.appendChild(parrafo);
-        
-            })
-
-        function eliminar(){
-            var elimina=document.getElementById("borra");
-                elimina.removeChild(holder);
-            };
-        eliminar();
-
-
-    }
-    listaTareas();
-    
-});
-
-*/
